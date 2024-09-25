67ea48ebf197996ae08e1ab1e6fc483498418892
AsmaeCharr <asmaecharroudd@gmail.com>
2024-09-25 16:15:13 +0200

    Added first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// ------------------------------------------------------------ CAT CLICKER 2 ------------------------------------------------------------
+let count1 = 0;
+let count2 = 0;
+
+document.getElementById("otter1").addEventListener("click", function () {
+    count1++;
+    document.getElementById("nums1").innerText = count1;
+});
+document.getElementById("otter2").addEventListener("click", function () {
+    count2++;
+    document.getElementById("nums2").innerText = count2;
+});
+
+// ------------------------------------------------------------ CAT CLICKER 1 ------------------------------------------------------------
+// let count =0;
+// function pet1() {
+//     count++;
+//     document.getElementById("nums1").innerText = count;
+// }
+
